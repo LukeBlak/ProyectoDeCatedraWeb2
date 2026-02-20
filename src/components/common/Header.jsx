522106d85ab48cc42d6b098337b2f1e925125929
Alejo <20245281@esen.edu.sv>
2026-02-20 14:13:13 -0600

    header color

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,11 +3,11 @@ import { Navbar } from './Navbar';
 
 export const Header = () => {
   return (
-    <header className="bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-500 shadow-lg">
+    <header className="bg-gradient-to-r from-scampi-800 via-purple-600 to-scampi-900 shadow-lg">
       {/* Barra de anuncio superior */}
-      <div className="bg-teal-400 text-gray-900 text-center py-2 px-4">
+      <div className="bg-teal-500 text-gray-900 text-center py-2 px-4">
         <p className="text-sm font-semibold">
-          🎉 ¡Ahorra hasta 70% en tus restaurantes favoritos! Cupones limitados
+          ¡Ahorra hasta 70% en tus restaurantes favoritos! Cupones limitados
         </p>
       </div>
 
